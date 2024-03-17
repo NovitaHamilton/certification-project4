a81477d15d57e7c63890eff96a97ff13c01dffd7
Novita H <novitahamilton@gmail.com>
2024-03-17 08:57:23 -0700

    Add browserService contiain localStorage helper

---
+++ client/src/services/browserService.js
@@ -0,0 +1,14 @@
+// Local Storage helper function
+
+const storeUser = (user) => {
+  localStorage.setItem('user', JSON.stringify(user));
+};
+
+const getUser = () => {
+  return JSON.parse(localStorage.getItem('user'));
+};
+const removeUser = () => {
+  return localStorage.removeItem(user);
+};
+
+export default { storeUser, getUser, removeUser };
